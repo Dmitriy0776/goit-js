bcb028e751fa3b9b4ded7a4b247c066f74ee623b
Dmitriy <velkov55@gmail.com>
2019-08-09 12:34:38 +0300

    corrections files hw-12

--- js/Homework-12/src/index.js
+++ js/Homework-12/src/index.js
@@ -5,36 +5,47 @@ import PNotify from 'pnotify/dist/es/PNotify';
 const form = document.querySelector('form');
 
 function getWeathcer(query) {
-  fetchWeather(query).then(data => {
-    console.log('data', data);
-    console.log(data.location.name);
-    document.querySelector('[class="icon"]').src = data.current.condition.icon;
-    document.querySelector('[class="icon"]').alt = data.current.condition.text;
-    document.querySelector('[data-field="location"]').textContent =
-      data.location.name;
-    document.querySelector('[data-field="temp"]').textContent = `${
-      data.current.temp_c
-    }\u2103`;
-    document.querySelector('[data-field="humidity"]').textContent = `${
-      data.current.humidity
-    }%`;
-    document.querySelector('[data-field="wind"]').textContent = `${
-      data.current.wind_kph
-    }kph`;
-    document.querySelector('[data-field="wind"]').textContent = `${
-      data.current.wind_kph
-    }kph`;
-    document.querySelector('[data-field="conditions"]').textContent = `${
-      data.current.condition.text
-    }`;
-    document.querySelector('.weather').classList.remove('is-hidden');
-  });
+  const spinner = document.querySelector('.spinner');
+  spinner.classList.add('active');
+  document.querySelector('.weather').classList.add('is-hidden');
+  setTimeout(
+    () =>
+      fetchWeather(query).then(data => {
+        console.log('data', data);
+        console.log(data.location.name);
+        document.querySelector('[class="icon"]').src =
+          data.current.condition.icon;
+        document.querySelector('[class="icon"]').alt =
+          data.current.condition.text;
+        document.querySelector('[data-field="location"]').textContent =
+          data.location.name;
+        document.querySelector('[data-field="temp"]').textContent = `${
+          data.current.temp_c
+        }\u2103`;
+        document.querySelector('[data-field="humidity"]').textContent = `${
+          data.current.humidity
+        }%`;
+        document.querySelector('[data-field="wind"]').textContent = `${
+          data.current.wind_kph
+        }kph`;
+        document.querySelector('[data-field="wind"]').textContent = `${
+          data.current.wind_kph
+        }kph`;
+        document.querySelector('[data-field="conditions"]').textContent = `${
+          data.current.condition.text
+        }`;
+
+        spinner.classList.remove('active');
+        document.querySelector('.weather').classList.remove('is-hidden');
+      }),
+    1500,
+  );
 }
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   getWeathcer(e.currentTarget.elements.city.value);
-})
+});
 
 getGeoPosition()
   .then(location => {
